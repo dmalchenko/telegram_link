98707cc19e5bb3bb26e75d0da73ce62f490fdce8
dmalchenko <malchenko.dmitriy.by@gmail.com>
2018-05-12 10:54:05 +0300

    fix bug with empty token

--- config/params.php
+++ config/params.php
@@ -5,5 +5,5 @@ return [
     'group_id' => '77253035',
     'vk_id' => '6212174',
     'vk_secret' => 'BTwwa8XUUbB0GcK6hbVd',
-    'vk_redirect_uri' => 'http://localhost:8888/vk/set-code',
+    'vk_redirect_uri' => 'http://tlegram.ru/vk/set-code',
 ];
